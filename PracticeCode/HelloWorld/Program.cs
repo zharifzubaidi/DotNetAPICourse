049ede9e05a46a0807066eb495d853d0d4878d57
zharifzubaidi <zharif96@gmail.com>
2024-11-07 15:08:05 +0800

    Basic theory update

---
+++ PracticeCode/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// WriteLine will include new line after display in powershell
+Console.WriteLine("Hello C# Programmer! Goodluck for your interview :)");
+// Write will not include new line after display in powershell
+Console.Write("First");
+Console.Write("Second");
