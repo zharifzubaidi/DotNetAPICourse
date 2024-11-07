45be87030ad64fd9aaacd78184cd7b55646ff105
zharifzubaidi <zharif96@gmail.com>
2024-11-07 15:19:02 +0800

    Lecture 2-17

--- PracticeCode/HelloWorld/Program.cs
+++ PracticeCode/HelloWorld/Program.cs
@@ -1,6 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// New template
+// See https://aka.ms/new-console-template for more information
 // WriteLine will include new line after display in powershell
-Console.WriteLine("Hello C# Programmer! Goodluck for your interview :)");
+//Console.WriteLine("Hello C# Programmer! Goodluck for your interview :)");
 // Write will not include new line after display in powershell
-Console.Write("First");
-Console.Write("Second");
+//Console.Write("First");
+//Console.Write("Second");
+
+// Old template
+
+using System;
+
+namespace HelloWorld{
+    internal class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
